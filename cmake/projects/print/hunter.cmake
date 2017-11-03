3f506ac28a18b2567ab4f62570d8b6035aa777a5
ToshUxanoff <tosha.uhanoff@yandex.ru>
2017-11-03 14:33:28 +0300

    added print package

---
+++ cmake/projects/print/hunter.cmake
@@ -0,0 +1,27 @@
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    print
+    VERSION
+    "0.1.0.0"
+    URL
+    "https://github.com/ToshUxanoff/lab09/archive/v0.1.0.0.tar.gz"
+    SHA1
+    f9050f22de5c8e9756f379b68521360d5ba866a3
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+
+hunter_cmake_args(
+    print
+    CMAKE_ARGS
+    BUILD_EXAMPLES=NO
+    BUILD_TESTS=NO
+)
+hunter_cacheable(print)
+hunter_download(PACKAGE_NAME print)
